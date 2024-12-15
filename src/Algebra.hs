fade50fb8f84625771287931cd27300ded9e75a7
GijsKUU <g.c.koppenberg@students.uu.nl>
2024-12-15 22:33:50 +0100

    5 & 6 denk ik
    Ook wat bugs in de lexer, model en parser gefixt

--- src/Algebra.hs
+++ src/Algebra.hs
@@ -7,44 +7,76 @@ import qualified Data.Set as Set
 
 
 -- Exercise 5
-type Algebra r c d p a = ( -- r :: Rule, c :: Cmd, d :: Dir, p :: Pat, a :: Alt
-    [r] -> r, -- Program
-    Func -> [c] -> r, -- Rule
-    c, --(GoCmd, TakeCmd, MarkCmd, NothingCmd)
-    d -> c, --TurnCmd
-    d -> [a] -> c, --CaseOfCmd
-    Func -> c, --FuncCmd
-    Dir -> d, --Dir
-    Pat -> [c] -> a, -- Alt
-    Pat -> p ) -- pat
 
-fold :: Algebra r c d p a -> Program -> r
-fold (mapPr, mapR, mapC, mapTC, mapCoC, mapFC, mapD, mapA, mapP) = foldProgram
-    where
-        foldProgram (Program rules) = mapPr (map foldRule rules)
-        -- map the foldRule on the [Rule]('rules') in Program
+type ProgramAlgebra r = ([Rule] -> r)
 
-        foldRule (Rule f cs) = mapR f (map foldCmd cs)
-        -- map foldCmd on [Cmd] and do mapR
+type RuleAlgebra r1 r2 = (Func -> [Cmd] -> r2)
 
+type FuncAlgebra r = (String -> r)
 
-        foldCmd GoCmd = mapC
-        foldCmd TakeCmd = mapC
-        foldCmd MarkCmd = mapC
-        foldCmd NothingCmd = mapC
-        foldCmd (TurnCmd d) = mapTC (foldDir d)
-        foldCmd (CaseOfCmd d as) = mapCoC (foldDir d) (map foldAlt as)
-        foldCmd (FuncCmd f) = mapFC f
-        
-        foldDir dir = mapD dir
+type CmdAlgebra r1 r2 r3 r4 r5 r6 r7 = (
+    r1,                   -- GoCmd
+    r1,                   -- TakeCmd
+    r1,                   -- MarkCmd
+    r1,                   -- NothingCmd
+    Dir -> r1,            -- TurnCmd
+    Dir -> [Alt] -> r1,   -- CaseOfCmd
+    Func -> r1            -- FuncCmd
+    )
 
-        foldAlt (Alt p cs) = mapA p (map foldCmd cs)
+type DirAlgebra r = (r, r, r)  -- left right front
 
-        foldPat pat = mapP pat
-   
+type AltAlgebra r1 r2 = (Pat -> [Cmd] -> r2)
+
+type PatAlgebra r = (
+    r,  -- empty
+    r,  -- lambda
+    r,  -- debris
+    r,  -- asteroid
+    r,  -- boundary
+    r   -- underscore
+    )
 
 
+foldProgram :: ProgramAlgebra r -> Program -> r
+foldProgram f (Program rules) = f rules
 
+foldRule :: RuleAlgebra r1 r2 -> Rule -> r2
+foldRule f (Rule func cmds) = f func cmds
+
+foldFunc :: FuncAlgebra r -> Func -> r
+foldFunc f (Functype str) = f str
+
+foldCmd :: CmdAlgebra r1 r2 r3 r4 r5 r6 r7 -> Cmd -> r1
+foldCmd (go, take, mark, nothing, turn, caseof, func) cmd = 
+    case cmd of
+        GoCmd -> go
+        TakeCmd -> take
+        MarkCmd -> mark
+        NothingCmd -> nothing
+        TurnCmd dir -> turn dir
+        CaseOfCmd dir alts -> caseof dir alts
+        FuncCmd f -> func f
+
+foldDir :: DirAlgebra r -> Dir -> r
+foldDir (left, right, front) dir =
+    case dir of
+        DirLeft -> left
+        DirRight -> right
+        DirFront -> front
+
+foldAlt :: AltAlgebra r1 r2 -> Alt -> r2
+foldAlt f (Alt pat cmds) = f pat cmds
+
+foldPat :: PatAlgebra r -> Pat -> r
+foldPat (empty, lambda, debris, asteroid, boundary, underscore) pat =
+    case pat of
+        EmptyPat -> empty
+        LambdaPat -> lambda
+        DebrisPat -> debris
+        AsteroidPat -> asteroid
+        BoundaryPat -> boundary
+        UnderscorePat -> underscore
 
 
 
@@ -52,87 +84,90 @@ fold (mapPr, mapR, mapC, mapTC, mapCoC, mapFC, mapD, mapA, mapP) = foldProgram
 
 -- main function to call to check program validity
 checkProgram :: Program -> Bool
-checkProgram program = let
-    finalState = fold checkAlgebra program initialState
-    in
-        allChecksPass finalState
-
-data ProgramInfo = ProgramInfo {
-    definedRules :: Set Func,   -- set of all defined rules
-    calledRules :: Set Func,    -- set of all called rules 
-    hasStart :: Bool,           -- has a start rule or not
-    hasDuplicates :: Bool,      -- has duplicate rules
-    noPatMatchFail :: Bool      -- no pattern matches can fail
-} deriving Show 
+checkProgram (Program rules) = 
+    checkStartCmd (Program rules) && 
+    checkRuleCalls (Program rules) &&
+    checkDoubleDefined (Program rules) && 
+    checkPatMatches rules  
 
 
+-- (1) check if the program has a start command
+{-
 
--- type CheckAlgebra = Algebra ProgramInfo () Dir () ()
+checkStartCmd :: ProgramAlgebra Bool 
+checkStartCmd [] = False
+checkStartCmd (x:xs) = giveFuncName x == "start" || checkStartCmd xs 
+-}
 
-initialState :: ProgramInfo
-initialState = ProgramInfo Set.empty Set.empty False False True
-
-
-checkAlgebra :: Algebra (ProgramInfo -> ProgramInfo) () Dir () ()
-checkAlgebra = (
-    checkProgram2,          -- all rules are correct
-    checkRule,              -- single rule follows constraints
-    (),                     -- normal commands don't have to be checked  (goCmd, takeCmd, etc.)
-    \_ -> (),               -- turnCmd doesn't need to be checken
-    checkCaseOfCmd,         -- checking constraint that all case ofs need to pattern match all 5 cases or have a catch all
-    \_ -> (),     -- keep track of all calls to functions that are made
-    id,                     -- just keep direction 
-    checkAlt,               -- check al Alt patterns
-    id                      -- keep pattern
-    )
-
-checkProgram2 :: [Rule] -> ProgramInfo -> ProgramInfo
-checkProgram2 rules state = foldl (flip checkRule) state rules
+-- Check if it has start command by folding over program
+checkStartCmd :: Program -> Bool 
+checkStartCmd = foldProgram (\rules -> any (\(Rule (Functype name) _) -> name == "start") rules)
 
 
-checkRule :: Rule -> ProgramInfo -> ProgramInfo
-checkRule (Rule f cmds) info = 
-    let state = 
-        -- if we find a "start" rule anywhere we turn hasStart to True
-            if f == Functype "start" then info { hasStart = True } else info
-        -- if the current rule is an already defined rule, we have duplicates
-        state2 = 
-            if Set.member f (definedRules state) then state {hasDuplicates = True} else state {definedRules = Set.insert f (definedRules state)} -- if the current rule isn't already defined, add it to the set of defined rules
-        in foldl (flip checkCmd) state2 cmds
+-- extract name of a function
+giveFuncName :: Rule -> String
+giveFuncName (Rule (Functype name) _ ) = name
 
 
+-- (2) check if all calles to rules are made to rules that actually exist
+checkRuleCalls :: Program -> Bool
+checkRuleCalls (Program rules) = 
+    let ruleNames = Set.fromList (getAllRuleNames rules)
+        calledRules = Set.fromList (getAllCalledRules rules)
+    in Set.isSubsetOf calledRules ruleNames
 
-checkCaseOfCmd :: Cmd -> ProgramInfo -> ProgramInfo
-checkCaseOfCmd (FuncCmd f) info = 
-    info {calledRules = Set.insert f (calledRules info)} -- keeping track of called rules, to see if all rules get called
-checkCaseOfCmd (CaseOfCmd _ alts) info = 
-    let 
-        patterns = map (\(Alt p _) -> p) alts -- get a list of all patterns, ignoring following commands
-        validPatterns = checkPatterns patterns
-    in 
-        info { noPatMatchFail = noPatMatchFail info && validPatterns }
-checkCmd _ info = info
 
--- check if all patterns are matched or the catch all is used 
-checkPatterns :: [Pat] -> Bool
-checkPatterns patterns = Set.isSubsetOf neededPats pats ||  elem UnderscorePat patterns -- are all needed patterns matched, or is the '_' catch all there
+getAllRuleNames :: ProgramAlgebra [String]
+getAllRuleNames rules = map (\(Rule (Functype name) _) -> name) rules
+
+getAllCalledRules :: ProgramAlgebra [String]
+getAllCalledRules [] = []
+getAllCalledRules (x:xs) = returnRuleCall (currentCommands)
     where
-        neededPats = Set.fromList [EmptyPat, LambdaPat, DebrisPat, AsteroidPat, BoundaryPat]
-        pats = Set.fromList patterns
+        currentCommands = getCmds x
+
+getCmds :: Rule -> [Cmd]
+getCmds (Rule _ cmds) = cmds
+
+returnRuleCall :: [Cmd] -> [String]
+returnRuleCall [] = []
+returnRuleCall (FuncCmd (Functype name) : xs) = name : returnRuleCall xs
+returnRuleCall (_:xs) = returnRuleCall xs
 
 
--- processing alts, used for processing commands
-checkAlt :: Alt -> ProgramInfo -> ProgramInfo
-checkAlt (Alt _ cmds) info = foldl (flip checkCaseOfCmd) info cmds
+-- (3) check if all rules are defined exactly ones
+checkDoubleDefined :: Program -> Bool -- if the size of the union of itself is the same size as the normal set there is no double values
+checkDoubleDefined (Program rules) = Set.size setrules == length rulesList
+    where
+        setrules = Set.fromList (getAllRuleNames rules)
+        rulesList = getAllRuleNames rules
 
 
 
-allChecksPass :: ProgramInfo -> Bool
-allChecksPass info =
-    hasStart info &&
-    not (hasDuplicates info) &&
-    Set.isSubsetOf (calledRules info) (definedRules info) &&
-    noPatMatchFail info
+-- (4) no ability to pattern match failure
+
+checkPatMatches :: [Rule] -> Bool
+checkPatMatches [] = True
+checkPatMatches ((Rule func cmds) : xs) = noPatMatchFail cmds && checkPatMatches xs
+
+noPatMatchFail :: [Cmd] -> Bool
+noPatMatchFail [] = True
+noPatMatchFail ((CaseOfCmd d a) : xs) = allPatsMatched (CaseOfCmd d a) && noPatMatchFail xs
+noPatMatchFail (_ : xs) = True && noPatMatchFail xs
+
+allPatsMatched :: Cmd -> Bool
+allPatsMatched (CaseOfCmd dir alts) = needed == pats || Set.member UnderscorePat pats
+    where
+        needed = Set.fromList ([EmptyPat, LambdaPat, DebrisPat, AsteroidPat, BoundaryPat, UnderscorePat])
+        pats = Set.fromList (getPatFromAlts alts)
+
+    
+getPatFromAlts :: [Alt] -> [Pat]
+getPatFromAlts alts = map (\(Alt pat _) -> pat) alts
+
+
+
+
 
 
 
